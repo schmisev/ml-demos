c5cb5c5ee3eb903aa7210a3d13681daa85f8d4f1
schmisev <schmisev@gmail.com>
2025-10-19 21:12:09 +0200

    some setup stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <div class="p-2">
   <h1>ML demos</h1>
+  <div>Disclaimer: these demos are in no way optimized and just here for my own amusement</div>
   <h2>Classfication</h2>
   <h3><a href="./kNN">kNN & DT</a></h3>
   <h2>Probabilistic Inference</h2>
